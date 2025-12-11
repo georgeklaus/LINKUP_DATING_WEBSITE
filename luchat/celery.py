5f4705438bdc5687eddad2f34bffe2e43e19a810
georgeklaus <164487115+georgeklaus@users.noreply.github.com>
2025-12-11 23:06:52 +0300

    Fix Celery app: use 'luchat.settings' and Celery('luchat')

--- luchat/celery.py
+++ luchat/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dating_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luchat.settings')
 
-app = Celery('dating_app')
+app = Celery('luchat')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
